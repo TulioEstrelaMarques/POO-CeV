0777e947e80743dce960ef14d465a926170ead3a
Túlio Estrela Marques <tulioestrela@hotmail.com>
2021-08-24 22:34:34 -0300

    Aula03

---
+++ Aula03/src/aula02/Caneta.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula02;
+
+/**
+ *
+ * @author tulio
+ */
+public class Caneta {
+
+    public String modelo, cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
+
+    public void status() {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta: " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tampanda? " + this.tampada);
+    }
+
+    public void rabiscar() {
+        if (this.tampada) {
+            System.out.println("ERRO! Não posso rabiscar");
+        } else {
+            System.out.println("Estou Rabiscando");
+        }
+    }
+
+    public void tampar() {
+        this.tampada = true;
+    }
+
+    public void destampar() {
+        this.tampada = false;
+    }
+}
