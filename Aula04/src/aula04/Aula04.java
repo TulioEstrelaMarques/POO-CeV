b67f6ad7dc4aadaf6d05798e4947238bd5491dca
Túlio Estrela Marques <tulioestrela@hotmail.com>
2021-08-25 12:01:45 -0300

    aula04

---
+++ Aula04/src/aula04/Aula04.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula04;
+
+/**
+ *
+ * @author tulio
+ */
+public class Aula04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NIC", "Amarelo", 0.4f);
+        Caneta c2 = new Caneta("KKK", "Laranja", 1.5f);
+
+        /**
+        *c1.setModelo("BIC");
+        *c1.modelo = "BIC2";
+        *c1.setPonta(0.5f);
+        *c1.ponta = 0.5f;
+        *System.out.println("Tenho uma caneta "+c1.getModelo()+" de ponta "+c1.getPonta());
+        */
+    }
+
+}
---
+++ Aula04/src/aula04/Caneta.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula04;
+
+/**
+ *
+ * @author tulio
+ */
+public class Caneta {
+
+    private String modelo, cor;
+    private float ponta;
+    private boolean tampada;
+
+    public Caneta(String modelo, String cor, float ponta) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.ponta = ponta;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public float getPonta() {
+        return ponta;
+    }
+
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+
+    public boolean isTampada() {
+        return tampada;
+    }
+
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
+    }
+
+}
