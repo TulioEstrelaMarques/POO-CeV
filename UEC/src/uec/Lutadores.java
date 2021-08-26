0c16c08ec54618b8766568f6060706efb4aaf833
Túlio Estrela Marques <tulioestrela@hotmail.com>
2021-08-25 23:49:43 -0300

    ainda não terminei aula 07

---
+++ UEC/src/uec/Lutadores.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uec;
+
+/**
+ *
+ * @author tulio
+ */
+public class Lutadores {
+
+    private String nome, nacionalidade;
+    private int idade;
+    private float altura, peso;
+    private String categoria;
+    private int vitorias, derrotas, empates;
+
+    public Lutadores(String nome, String nacionalidade, int idade, float altura, float peso, int vitorias, int derrotas, int empates) {
+        this.setNome(nome);
+        this.setNacionalidade(nacionalidade);
+        this.setIdade(idade);
+        this.setAltura(altura);
+        this.setPeso(peso);
+        this.setVitorias(vitorias);
+        this.setDerrotas(derrotas);
+        this.setEmpates(empates);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+        setCategoria();
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    private void setCategoria() {
+        if (this.getPeso() < 52.2) {
+            this.categoria = "Inválido";
+        } else if (this.getPeso() <= 70.3) {
+            this.categoria = "Leve";
+        } else if (this.getPeso() <= 83.9) {
+            this.categoria = "Médio";
+        } else if (this.getPeso() <= 120.2) {
+            this.categoria = "Pesado";
+        } else {
+            this.categoria = "Inválido";
+        }
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+    public void apresentar() {
+        System.out.println("Senhoras e senhores... CHEGOU O TÃO AGUARDADO MOMENTO! Apresentamos o lutador: " + this.getNome());
+        System.out.println("Diretamente de: " + this.getNacionalidade());
+        System.out.println("com " + this.getIdade() + " anos e " + this.getAltura() + " m de altura");
+        System.out.println("pesando " + this.getPeso() + "Kg");
+        System.out.println("Carregando contigo : " + this.getVitorias() + " Vitórias");
+        System.out.println(this.getDerrotas() + " Derrotas e");
+        System.out.println(this.getEmpates() + "Empates!");
+    }
+
+    public void status() {
+        System.out.println(this.getNome() + " é um peso " + this.getCategoria());
+        System.out.println("Ganhou: " + this.getVitorias() + " vezes");
+        System.out.println("Perdeu: " + this.getDerrotas() + " vezes");
+        System.out.println("Empatou: " + this.getEmpates() + " vezes");
+    }
+
+    public void ganharLuta() {
+        this.setVitorias(this.getVitorias() + 1);
+    }
+
+    public void perderLuta() {
+        this.setDerrotas(this.getDerrotas() + 1);
+    }
+
+    public void empatarLuta() {
+        this.setEmpates(this.getEmpates() + 1);
+    }
+}
